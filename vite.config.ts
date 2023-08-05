6c2f3fca1e2c9538410fbcfa1dabb2e2dd7d46e2
张艺璇 <635010314@qq.com>
2023-08-05 17:49:09 +0800

    注释

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import * as path from 'path';
-import {
-    createStyleImportPlugin,
-    ElementPlusResolve,
-} from 'vite-plugin-style-import';
+// import {
+//     createStyleImportPlugin,
+//     ElementPlusResolve,
+// } from 'vite-plugin-style-import';
 
 // https://vitejs.dev/config/
 export default defineConfig({
